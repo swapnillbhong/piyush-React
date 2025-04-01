6a77155b494153ca949c22dbd14a6ed822904cd2
swapnillbhong <swapnilbhong1627@gmail.com>
2025-04-01 19:00:38 +0530

    comment part is the use of hooks

--- hooksinReact/src/App.jsx
+++ hooksinReact/src/App.jsx
@@ -2,20 +2,20 @@
 import './App.css'
 import { useEffect , useState } from 'react'
 
-import Mycomponents from './Components/Mycomponents'
+// import Mycomponents from './Components/Mycomponents'
 
 function App() {
 
-  const [isVisible, setVisible]= useState(true)
+  // const [isVisible, setVisible]= useState(true)
   
-  useEffect(()=>{
-    console.log("App component is mounting  ...... ")
-  },[])
+  // useEffect(()=>{
+  //   console.log("App component is mounting  ...... ")
+  // },[])
 
   return (
     <>
-      {isVisible?<Mycomponents/>: <></>} 
-      <button onClick={()=> setVisible (!isVisible)}> Toggle</button>
+      {/* {isVisible?<Mycomponents/>: <></>} 
+      <button onClick={()=> setVisible (!isVisible)}> Toggle</button> */}
     </>
   )
 }
