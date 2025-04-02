eb9b3c19d94ee9f3abb01025ded3495ae19f3a38
swapnillbhong <swapnilbhong1627@gmail.com>
2025-04-02 12:47:39 +0530

    incomplete push

---
+++ tic-tac-toe/src/components/Board.jsx
@@ -0,0 +1,35 @@
+import React,{useState} from "react";
+
+import Square from "./Square";
+    
+const Board = () => {
+
+    const [state,setState] = useState(Array(9).fill(null));
+  
+    const handleClick =(index) =>{
+        console.log("click on index",index)
+    }
+
+
+  return (
+    <div className="Board-container bor ">
+        <div className="Board-row bor">
+            <Square onClick={()=> handleClick(0)} value={state[0]}/>
+            <Square onClick={()=> handleClick(1)} value={state[1]}/>
+            <Square onClick={()=> handleClick(2)} value={state[2]}/>
+        </div>
+        <div className="Board-row">
+        <Square onClick={()=> handleClick(3)} value={state[3]}/>
+        <Square onClick={()=> handleClick(4)} value={state[4]}/>
+        <Square onClick={()=> handleClick(5)} value={state[5]}/>
+        </div>
+        <div className="Board-row">
+        <Square onClick={()=> handleClick(6)} value={state[6]}/>
+        <Square onClick={()=> handleClick(7)} value={state[7]}/>
+        <Square onClick={()=> handleClick(8)} value={state[8]}/>
+        </div>
+    </div>
+  )
+}
+
+export default Board
