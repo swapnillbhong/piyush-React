9c140db504d1fcc70f15a05af8f5c9cc488c978a
Swapnil Bhong <99287395+swapnillbhong@users.noreply.github.com>
2025-04-03 07:06:44 +0000

    winner logic

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -8,16 +8,42 @@ const Board = () => {
   
     const [isXTurn, setIsXTurn] = useState(true);
 
+    const CheckWinner= () =>{
+      const winnerLogic = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6], 
+        [1,4,7] ,
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]  
+       ];
+
+       for(let logic of winnerLogic){
+        const [ a, b, c] =  logic;
+        if(state[a] !==null && state[a]=== state[b]  && state[a] === state[c]) 
+        {
+          return true;
+        }
+       }
+        return false;
+    };
+
+    const isWinner =CheckWinner();
+
     const handleClick =(index) =>{
       const copyState = [...state];
-      copyState[index] = isXTurn ? "'X'" :"'0'";
+      copyState[index] = isXTurn ? "X" :"O";
       setState(copyState);
+      setIsXTurn(!isXTurn);
     }
 
 
   return (
     <div className="Board-container bor ">
-        <div className="Board-row bor">
+        {isWinner?(<>Someone won</>):(<>
+          <div className="Board-row bor">
             <Square onClick={()=> handleClick(0)} value={state[0]}/>
             <Square onClick={()=> handleClick(1)} value={state[1]}/>
             <Square onClick={()=> handleClick(2)} value={state[2]}/>
@@ -32,6 +58,7 @@ const Board = () => {
         <Square onClick={()=> handleClick(7)} value={state[7]}/>
         <Square onClick={()=> handleClick(8)} value={state[8]}/>
         </div>
+        </>)}
     </div>
   )
 }
